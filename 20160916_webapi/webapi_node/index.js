859f5b3f5d8769c1a8dd8ff25e3dd4b0bc780ea1
yamap55 <tetuya120@hotmail.com>
2016-09-14 00:28:04 +0900

    [add] Access-Control-Allow-Origin

--- 20160916_webapi/webapi_node/index.js
+++ 20160916_webapi/webapi_node/index.js
@@ -6,7 +6,7 @@ const con = "tcp://postgres:postgres@localhost/webapi-example";
 server.get('/', function(req, res){
   const name = req.query.name;
   const password = req.query.pass;
-  console.log(name + " : " + password);
+  console.log("input name:" + name + ", password:" + password);
   if (!name || !password) {
     res.json({});
     return;
@@ -25,6 +25,9 @@ server.get('/', function(req, res){
         console.log(err);
         return;
       }
+      const mes = client.rows.length < 1 ? "id not found" : "success id:" + client.rows[0].id + ", name:" + client.rows[0].name;
+      console.log(mes);
+      res.header({"Access-Control-Allow-Origin":"*"});
       res.json(client.rows);
     });
   });
