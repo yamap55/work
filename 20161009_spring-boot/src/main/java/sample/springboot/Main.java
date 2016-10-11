09820aa7d603d4a3fe5b642ec38d2e9d170221f8
yamap55 <tetuya120@hotmail.com>
2016-10-11 23:47:11 +0900

    [add] spring boot web application

--- 20161009_spring-boot/src/main/java/sample/springboot/Main.java
+++ 20161009_spring-boot/src/main/java/sample/springboot/Main.java
@@ -2,15 +2,11 @@ package sample.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        try (ConfigurableApplicationContext ctx = SpringApplication.run(Main.class, args)) {
-            Hoge h = ctx.getBean(Hoge.class);
-            System.out.println(h);
-        }
+        SpringApplication.run(Main.class, args);
     }
 }
---
+++ 20161009_spring-boot/src/main/java/sample/springboot/web/HelloController.java
@@ -0,0 +1,15 @@
+package sample.springboot.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping(method=RequestMethod.GET)
+    public String hello() {
+        return "Hello Spring MVC";
+    }
+}
