5a6642a1f08e09c7b953135edc8b2f9908675eb7
yamap55 <tetuya120@hotmail.com>
2016-11-13 23:42:04 +0900

    [add] geb code

---
+++ 20161118_geb/code.groovy
@@ -0,0 +1,15 @@
+@Grapes(
+    @Grab(group='org.gebish', module='geb-core', version='1.0')
+)
+import geb.Browser
+
+Browser.drive {
+    go "http://gebish.org"
+
+    assert title == "Geb - Very Groovy Browser Automation" 
+
+    $("#sidebar .sidemenu a", text: "jQuery-like API").click()
+
+    assert $("#main h1")*.text() == ["Navigating Content", "Form Control Shortcuts"] 
+    assert $("#sidebar .sidemenu a", text: "jQuery-like API").parent().hasClass("selected")
+}
