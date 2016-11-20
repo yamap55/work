9262320766b91e7d385958b4e765e6e9f283b30b
yamap55 <tetuya120@hotmail.com>
2016-11-20 23:06:56 +0900

    [update] selenium select xpath

--- 20161118_geb/selenium.groovy
+++ 20161118_geb/selenium.groovy
@@ -15,20 +15,20 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public static void main(String[] args) {
   println "start ${new Date().format('yyyy/MM/dd HH:mm:ss')}"
-  
+
   System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe")
   ChromeDriver driver = new ChromeDriver()
   driver.get("http://gebish.org");
   WebDriverWait<WebDriver> wait = new WebDriverWait(driver, 30);
-  
+
   System.out.println(driver.getTitle())
-  
+
   WebElement elem = driver.findElement(By.id("main"));
   List<WebElement> list = elem.findElements(new By.ByTagName("h1"));
   for (WebElement e : list) {
     System.out.println(e.getText())
   }
-  
+
   WebElement sidebar = driver.findElement(By.id("sidebar"));
   List<WebElement> sidemenus = driver.findElements(new By.ByClassName("sidemenu"));
   for (WebElement sidemenu : sidemenus) {
@@ -45,7 +45,9 @@ public static void main(String[] args) {
   for (WebElement e : list2) {
     System.out.println(e.getText())
   }
-  
+
+// TODO
+//  driver.findElement(By.xpath("//input[@name='hoge' and @value='on']")).click();
 
   println "end ${new Date().format('yyyy/MM/dd HH:mm:ss')}"
 }
