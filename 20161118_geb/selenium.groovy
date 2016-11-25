48c3e3726c382aa097e9c66989d044010d836754
yamap55 <tetuya120@hotmail.com>
2016-11-26 03:22:29 +0900

    [update] minor fixed

--- 20161118_geb/selenium.groovy
+++ 20161118_geb/selenium.groovy
@@ -16,26 +16,36 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public static void main(String[] args) {
   println "start ${new Date().format('yyyy/MM/dd HH:mm:ss')}"
 
-  System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe")
-  ChromeDriver driver = new ChromeDriver()
+  System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+  ChromeDriver driver = new ChromeDriver();
   driver.get("http://gebish.org");
   WebDriverWait<WebDriver> wait = new WebDriverWait(driver, 30);
 
-  System.out.println(driver.getTitle())
+  System.out.println(driver.getTitle());
 
   WebElement elem = driver.findElement(By.id("main"));
   List<WebElement> list = elem.findElements(new By.ByTagName("h1"));
   for (WebElement e : list) {
-    System.out.println(e.getText())
+    System.out.println(e.getText());
   }
+
+  println ("*"*10)
+
+  // XPATHでの指定も可能「//div[@id='main']//h1」
+  List<WebElement> lista =   driver.findElements(By.xpath("//div[@id='main']//h1"));
+  for (WebElement e : lista) {
+    System.out.println(e.getText());
+  }
+
+  println ("*"*10)
 
   WebElement sidebar = driver.findElement(By.id("sidebar"));
   List<WebElement> sidemenus = driver.findElements(new By.ByClassName("sidemenu"));
   for (WebElement sidemenu : sidemenus) {
-    List<WebElement> links = sidemenu.findElements(new By.ByTagName("a"))
+    List<WebElement> links = sidemenu.findElements(new By.ByTagName("a"));
     for (WebElement link : links) {
       if (link.getText().equals("jQuery-like API")) {
-        link.click()
+        link.click();
         break;
       }
     }
@@ -43,11 +53,8 @@ public static void main(String[] args) {
   WebElement elem2 = driver.findElement(By.id("main"));
   List<WebElement> list2 = elem2.findElements(new By.ByTagName("h1"));
   for (WebElement e : list2) {
-    System.out.println(e.getText())
+    System.out.println(e.getText());
   }
-
-// TODO
-//  driver.findElement(By.xpath("//input[@name='hoge' and @value='on']")).click();
 
   println "end ${new Date().format('yyyy/MM/dd HH:mm:ss')}"
 }
