a20f3757e4a9ab1866ffdd08546b92a6a37d7bea
yamap55 <tetuya120@hotmail.com>
2017-04-26 17:20:45 +0900

    [add] sepalate players script

---
+++ 20170425_twithitter/playerSepalater.groovy
@@ -0,0 +1,21 @@
+// あ
+// def path = "/Users/yamap_55/Desktop/twithitter_memo.txt"
+def path = "/Users/yamap_55/Desktop/twithitter_pitcher.txt"
+// def path = "/Users/yamap_55/Desktop/twithitter_batter.txt"
+def f = new File(path)
+
+def i = 0
+f.text.eachLine {
+  def m = it =~ /\[(.*)\]/
+  def l = m[0][1].split(",").collect{it.trim()}
+
+  // 投手で先発以外
+  def stamina = l.remove(2)
+  if (stamina == "Good?") {
+    return
+  }
+  // 投手で先発以外 ここまで
+  if(l.every{it == "Good?"}) {
+    println it
+  }
+}
