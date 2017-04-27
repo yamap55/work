7f11af82e58a7b064bd9722c8d1502731bffd298
yamap55 <tetuya120@hotmail.com>
2017-04-28 01:02:56 +0900

    [add] twithitter script

---
+++ 20170427_twithitter_geb/GebConfig.groovy
@@ -0,0 +1,16 @@
+@Grapes([
+    @Grab("org.gebish:geb-core:1.1.1"),
+    @Grab("com.codeborne:phantomjsdriver:1.4.2"),
+    @Grab("org.seleniumhq.selenium:selenium-chrome-driver:3.4.0")
+])
+
+import org.openqa.selenium.chrome.ChromeDriver
+
+System.setProperty("webdriver.chrome.driver", "./driver/chromedriver")
+
+driver = {
+    return new ChromeDriver()
+}
+
+loginTwitterId = "****"
+loginTwitterPassword = "****"
---
+++ 20170427_twithitter_geb/code.groovy
@@ -0,0 +1,160 @@
+@Grapes([
+    @Grab("org.gebish:geb-core:1.1.1"),
+    @Grab("org.seleniumhq.selenium:selenium-support:3.4.0"),
+])
+
+import geb.Browser
+import groovy.transform.ToString
+
+
+def batterResultPath = "/Users/yamap_55/Desktop/twithitter_batter.txt"
+def pitcherResultPath = "/Users/yamap_55/Desktop/twithitter_pitcher.txt"
+
+println "start ${new Date().format('yyyy/MM/dd HH:mm:ss')}"
+
+def batterResultFile = new File(batterResultPath)
+def pitcherResultFile = new File(pitcherResultPath)
+
+def num = 100000
+
+def createRandomStr = {
+  def array = (0..9)+('a'..'z')+'_'
+  def f = {
+    new Random().with { (1..4).collect { array[nextInt(array.size())] }.join() }
+  }
+}()
+
+def u = "https://twithitter.com/"
+def errorCount = 0
+
+Browser.drive {
+  go "https://twithitter.com/login"
+  $("#username_or_email").value(browser.config.rawConfig.loginTwitterId)
+  $("#password").value(browser.config.rawConfig.loginTwitterPassword)
+  $(".submit")[0].click()
+  waitFor{ title == "TwitHitter" }
+  num.times {
+    if (!((it + 1) % 10)) {
+      println "${new Date().format('yyyy/MM/dd HH:mm:ss')}, ${it+1}/${num}, error : ${errorCount}"
+    }
+    go "https://twithitter.com/${createRandomStr()}"
+    def batterStatusArea = $(".batter-status")
+    def pitcherStatusArea = $(".pitcher-status")
+    if (!batterStatusArea && !pitcherStatusArea) {
+      // 存在しないIDの場合
+      errorCount++
+      return
+    }
+    def type = batterStatusArea ? "打者" : "投手"
+    def user = new User($(".player-profile")[0], batterStatusArea, pitcherStatusArea)
+
+    // def area = pitcherStatusArea[0]
+    // // 球速、制球、スタミナ、残りは変化球
+    // // AとかBとかのランクは.rank-labelで取れるが、球速はなし。
+    // def status = area.find(".status-val")*.text()
+    // println status
+//    println user
+    if (user.status.isTarget()) {
+      def r = "${user.twitterId} : ${user.type} : ${user.status}"
+      println r
+      def result = user.isBatter() ? batterResultFile : pitcherResultFile
+      result << "${r}\n"
+    }
+  }
+}
+
+println "end ${new Date().format('yyyy/MM/dd HH:mm:ss')}"
+
+@ToString(excludes = ["profileArea", "rank"])
+class User {
+  def profileArea
+  def name
+  def twitterId
+  def throwBat
+  def team
+  def popularity
+
+  def type
+  def status
+
+  def getSimpleStatus() {
+    isBatter()? status[0..4] : status[0..2]
+  }
+
+  def isPitcher() {
+    type == "投手"
+  }
+
+  def isBatter() {
+    type == "打者"
+  }
+
+  User(profileArea, batterStatusArea, pitcherStatusArea) {
+    def f = {
+      profileArea.$(".${it}")[0].text()
+    }
+    name = f("name")
+    twitterId = f("screen-name")
+    throwBat = f("throw-bat")
+    team = f("userteam-none-label")
+    popularity = profileArea.find(".userteam-popularity")[0].text().split(" ")[1]
+
+    type = batterStatusArea ? "打者" : "投手"
+    if (batterStatusArea) {
+      // 打者の場合
+      def area = batterStatusArea[0]
+      def s = area.find(".left-column")[0].find(".status-val")*.text()
+      status = new Status(type, s)
+    } else {
+      // 投手の場合
+      def area = pitcherStatusArea[0]
+      def s = area.find(".status-val")*.text()
+      status = new Status(type, s)
+    }
+  }
+}
+
+@ToString(excludes = ["type"])
+class Status {
+  def value = [:]
+  def type
+  Status(t, status) {
+    type = t
+    if (type == "打者") {
+      value["ミート"] = status[0] as int
+      value["パワー"] = status[1] as int
+      value["走力"] = status[2] as int
+      value["肩力"] = status[3] as int
+      value["捕"] = status[4] as int
+      value["一"] = status[5] as int
+      value["二"] = status[6] as int
+      value["三"] = status[7] as int
+      value["遊"] = status[8] as int
+      value["左"] = status[9] as int
+      value["中"] = status[10] as int
+      value["右"] = status[11] as int
+    } else {
+      value["球速"] = status[0].split(" ")[0] as int
+      value["制球"] = status[1] as int
+      value["スタミナ"] = status[2] as int
+      value["変化"] = status[3..status.size()-1].collect{it as int}
+    }
+  }
+
+  def isTarget() {
+    if (type == "打者") {
+      return [value["ミート"],value["パワー"],value["走力"],value["肩力"]].every{it >= 80}
+    } else {
+      if (value["球速"] >= 145 && value["制球"] >= 80) {
+        if (value["スタミナ"] >= 80) {
+          // 先発
+          return value["変化"].every{it >= 70 }
+        } else {
+          // 中継ぎ
+          return value["変化"].every{it >= 80 }
+        }
+      }
+      return false
+    }
+  }
+}
