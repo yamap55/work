8122732e5a2978753f5e1e3636dd0ac68dc82382
yamap55 <tetuya120@hotmail.com>
2017-10-14 02:31:03 +0900

    [update] refactoring

--- 20171007_twitter/Main.groovy
+++ 20171007_twitter/Main.groovy
@@ -1,4 +1,4 @@
-@Grab(group='org.twitter4j', module='twitter4j-core', version='4.0.6')
+@Grab("org.twitter4j:twitter4j-core:4.0.6")
 @Grab("org.codehaus.groovy.modules.http-builder:http-builder:0.7.1")
 @Grab("org.apache.httpcomponents:httpmime:4.5.3")
 
