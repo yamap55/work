37469510109e823a349885cda616dfe5326d0b40
yamap55 <tetuya120@hotmail.com>
2017-10-14 02:38:32 +0900

    [add] upload image to gyazo

--- 20171007_twitter/Main.groovy
+++ 20171007_twitter/Main.groovy
@@ -13,6 +13,11 @@ class Main {
     def captureInfo = selenide.getCapture("https://weather.yahoo.co.jp/weather/13/4410.html", [".forecastCity","div"])
     println captureInfo
 
+    def gyazo = new Gyazo("token")
+    def url = gyazo.upload(captureInfo.file,)
+
+    println url
+
     def twitterMessage = new TwitterExecuter().tweetSearch("from:Yahoo_weather 東京の天気")
     println twitterMessage
   }
