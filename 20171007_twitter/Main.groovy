0ee2fee9719a729ca34561bb0abb3b1bcf4f8f53
yamap55 <tetuya120@hotmail.com>
2017-10-15 01:43:59 +0900

    [add] post weather with slack bot

--- 20171007_twitter/Main.groovy
+++ 20171007_twitter/Main.groovy
@@ -3,25 +3,33 @@
   @Grab("org.twitter4j:twitter4j-core:4.0.6"),
   @Grab("org.codehaus.groovy.modules.http-builder:http-builder:0.7.1"),
   @Grab("org.apache.httpcomponents:httpmime:4.5.3"),
+  @Grab("org.riversun:slacklet:1.0.1")
 ])
 
 import twitter4j.*
 
+import org.riversun.slacklet.*
+import org.riversun.xternal.simpleslackapi.*
+
 class Main {
   static void main(args) {
-    def selenide = new SelenideExecuter()
-    def captureInfo = selenide.getCapture("https://weather.yahoo.co.jp/weather/13/4410.html", [".forecastCity","div"])
-    println captureInfo
+    // def selenide = new SelenideExecuter()
+    // def captureInfo = selenide.getCapture("https://weather.yahoo.co.jp/weather/13/4410.html", [".forecastCity","div"])
+    // println captureInfo
+    //
+    // // TODO Macの場合には2倍する必要あり。理由わからず。
+    // def cap = ImageEditer.subImage(captureInfo.file, captureInfo.x * 2, captureInfo.y * 2, captureInfo.width * 2, captureInfo.height * 2)
+    //
+    // def gyazo = new Gyazo("token")
+    // def url = gyazo.upload(cap)
+    //
+    // println url
+    //
+    // def twitterMessage = new TwitterExecuter().tweetSearch("from:Yahoo_weather 東京の天気")
+    // println twitterMessage
+    def slack = new SlackJoin("token")
+    slack.execute()
+    slack.start()
 
-    // TODO Macの場合には2倍する必要あり。理由わからず。
-    def cap = ImageEditer.subImage(captureInfo.file, captureInfo.x * 2, captureInfo.y * 2, captureInfo.width * 2, captureInfo.height * 2)
-
-    def gyazo = new Gyazo("token")
-    def url = gyazo.upload(cap)
-
-    println url
-
-    def twitterMessage = new TwitterExecuter().tweetSearch("from:Yahoo_weather 東京の天気")
-    println twitterMessage
   }
 }
--- 20171007_twitter/SlackJoin.groovy
+++ 20171007_twitter/SlackJoin.groovy
@@ -1,17 +1,25 @@
 // 参考
 // https://qiita.com/riversun/items/25f64f285699223a992d
 
-@Grapes(
-    @Grab(group='org.riversun', module='slacklet', version='1.0.1')
-)
 import org.riversun.slacklet.*
 import org.riversun.xternal.simpleslackapi.*
 
 class SlackJoin {
-  def token = System.getenv()["SLACK_TOKEN"]
-  def execute() {
-    SlackletService slackService = new SlackletService(token)
+  def slackService
+  SlackJoin(token) {
+    this.slackService = new SlackletService(token)
+  }
 
+  def start() {
+    // slackletserviceを開始(slackに接続)
+    slackService.start();
+  }
+
+  def stop() {
+    slackService.stop();
+  }
+
+  def execute() {
     // slackletを追加する
     slackService.addSlacklet(new Slacklet() {
       @Override
@@ -19,25 +27,46 @@ class SlackJoin {
         // メッセージがユーザーからポストされた
         // メッセージ本文を取得
         String content = req.getContent();
+        def sender = req.sender
+        if (sender.realName == "slackbot") {
+          return
+        }
         // メッセージがポストされたチャンネルに対して、BOTからメッセージを送る
-        resp.reply("「" + content + "」て言いましたね。");
+        resp.reply("「${sender.realName}」が「${content}」て言いましたね。");
       }
 
       @Override
       public void onMentionedMessagePosted(SlackletRequest req, SlackletResponse resp) {
         String content = req.getContent();
-        resp.reply("「" + content + "」て言いましたね。");
+        def sender = req.sender
+        if (sender.realName == "slackbot") {
+          return
+        }
+        resp.reply("「${sender.realName}」が「${content}」て話しかけましたね。");
         if (content.contains("stop")) {
           resp.reply("「stop」します。");
-          slackService.stop();
-          System.exit(0)
+          stop();
+          // System.exit(0)
         } else if (content.contains("天気")) {
+          def selenide = new SelenideExecuter()
+          def captureInfo = selenide.getCapture("https://weather.yahoo.co.jp/weather/13/4410.html", [".forecastCity","div"])
+          // println captureInfo
 
+          // TODO Macの場合には2倍する必要あり。理由わからず。
+          def cap = ImageEditer.subImage(captureInfo.file, captureInfo.x * 2, captureInfo.y * 2, captureInfo.width * 2, captureInfo.height * 2)
+
+          def gyazo = new Gyazo("token")
+          def url = gyazo.upload(cap)
+          // println url
+
+          def twitterMessage = new TwitterExecuter().tweetSearch("from:Yahoo_weather 東京の天気")
+          // println twitterMessage
+
+          resp.reply("${twitterMessage}\n${url}");
+          println req.channel.name
         }
       }
     });
 
-    // slackletserviceを開始(slackに接続)
-    slackService.start();
   }
 }
